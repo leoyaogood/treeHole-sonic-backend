922807b722ebb5e0958c4051e6d5e598c21972d2
liqiqiorz <1099462011@qq.com>
2020-07-23 01:59:18 +0800

    增加模板依赖

--- src/main/java/com/yxr/weizhi/treehole/controller/CommentController.java
+++ src/main/java/com/yxr/weizhi/treehole/controller/CommentController.java
@@ -6,11 +6,10 @@ import com.yxr.weizhi.treehole.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- src/main/java/com/yxr/weizhi/treehole/service/impl/CommentServiceImpl.java
+++ src/main/java/com/yxr/weizhi/treehole/service/impl/CommentServiceImpl.java
@@ -1,10 +1,14 @@
 package com.yxr.weizhi.treehole.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yxr.weizhi.treehole.entity.Comment;
 import com.yxr.weizhi.treehole.mapper.CommentMapper;
 import com.yxr.weizhi.treehole.service.CommentService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +20,66 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CommentServiceImpl extends ServiceImpl<CommentMapper, Comment> implements CommentService {
+    @Autowired
+    private CommentDao commentDao;
 
+    //存放迭代找出的所有子代的集合
+    private List<Comment> tempReplys = new ArrayList<>();
+
+
+    @Override
+    public List<Comment> listComment() {
+        //查询出父节点
+        List<Comment> comments = commentDao.findByParentIdNull(Long.parseLong("-1"));
+        for(Comment comment : comments){
+            Long id = comment.getId();
+            String parentNickname1 = comment.getNickname();
+            List<Comment> childComments = commentDao.findByParentIdNotNull(id);
+            //查询出子评论
+            combineChildren(childComments, parentNickname1);
+            comment.setReplyComments(tempReplys);
+            tempReplys = new ArrayList<>();
+        }
+        return comments;
+    }
+
+
+    private void combineChildren(List<Comment> childComments, String parentNickname1) {
+        //判断是否有一级子回复
+        if(childComments.size() > 0){
+            //循环找出子评论的id
+            for(Comment childComment : childComments){
+                String parentNickname = childComment.getNickname();
+                childComment.setParentNickname(parentNickname1);
+                tempReplys.add(childComment);
+                Long childId = childComment.getId();
+                //查询二级以及所有子集回复
+                recursively(childId, parentNickname);
+            }
+        }
+    }
+
+
+    private void recursively(Long childId, String parentNickname1) {
+        //根据子一级评论的id找到子二级评论
+        List<Comment> replayComments = commentDao.findByReplayId(childId);
+
+        if(replayComments.size() > 0){
+            for(Comment replayComment : replayComments){
+                String parentNickname = replayComment.getNickname();
+                replayComment.setParentNickname(parentNickname1);
+                Long replayId = replayComment.getId();
+                tempReplys.add(replayComment);
+                //循环迭代找出子集回复
+                recursively(replayId,parentNickname);
+            }
+        }
+    }
+
+    @Override
+    //存储评论信息
+    public int saveComment(Comment comment) {
+        comment.setCreateTime(new Date());
+        return commentDao.saveComment(comment);
+    }
 }
