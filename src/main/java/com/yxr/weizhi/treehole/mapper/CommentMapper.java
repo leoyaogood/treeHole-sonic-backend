44680b77f6143d9eb9561acc6c21d75477be8151
liqiqiorz <1099462011@qq.com>
2020-07-23 01:48:05 +0800

    评论接口实现

--- src/main/java/com/yxr/weizhi/treehole/mapper/CommentMapper.java
+++ src/main/java/com/yxr/weizhi/treehole/mapper/CommentMapper.java
@@ -2,6 +2,11 @@ package com.yxr.weizhi.treehole.mapper;
 
 import com.yxr.weizhi.treehole.entity.Comment;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,6 +16,19 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author liqiqiorz
  * @since 2020-07-23
  */
+@Mapper
+@Repository
 public interface CommentMapper extends BaseMapper<Comment> {
 
+    //添加一个评论
+    int saveComment(Comment comment);
+
+    //查询父级评论
+    List<Comment> findByParentIdNull(@Param("ParentId") Long ParentId);
+
+    //查询一级回复
+    List<Comment> findByParentIdNotNull(@Param("id") Long id);
+
+    //查询二级以及所有子集回复
+    List<Comment> findByReplayId(@Param("childId") Long childId);
 }
--- src/main/java/com/yxr/weizhi/treehole/service/CommentService.java
+++ src/main/java/com/yxr/weizhi/treehole/service/CommentService.java
@@ -3,6 +3,8 @@ package com.yxr.weizhi.treehole.service;
 import com.yxr.weizhi.treehole.entity.Comment;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -13,4 +15,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface CommentService extends IService<Comment> {
 
+
+    //查询评论列表
+    List<Comment> listComment();
+
+    //保存评论
+    int saveComment(Comment comment);
 }
