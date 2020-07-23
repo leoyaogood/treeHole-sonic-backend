886d272f3cdca190769a0a1c2266ddc238cfa2f2
liqiqiorz <1099462011@qq.com>
2020-07-24 00:53:54 +0800

    依赖异常

--- src/main/java/com/yxr/weizhi/treehole/hander/ControllerExceptionHandler.java
+++ src/main/java/com/yxr/weizhi/treehole/hander/ControllerExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.yxr.weizhi.treehole.hander;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
