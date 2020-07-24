71ea613b55098ace0483646aaf6df0f5cfcb58ae
liqiqiorz <1099462011@qq.com>
2020-07-24 21:17:57 +0800

    用户API

--- src/main/java/com/yxr/weizhi/treehole/service/impl/UserServiceImpl.java
+++ src/main/java/com/yxr/weizhi/treehole/service/impl/UserServiceImpl.java
@@ -1,12 +1,11 @@
 package com.yxr.weizhi.treehole.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.yxr.weizhi.treehole.entity.Comment;
 import com.yxr.weizhi.treehole.entity.User;
-import com.yxr.weizhi.treehole.mapper.CommentMapper;
 import com.yxr.weizhi.treehole.mapper.UserMapper;
-import com.yxr.weizhi.treehole.service.CommentService;
 import com.yxr.weizhi.treehole.service.UserService;
+import com.yxr.weizhi.treehole.utils.MD5Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author liqiqi_tql
@@ -14,4 +13,13 @@ import com.yxr.weizhi.treehole.service.UserService;
  */
 public class UserServiceImpl extends ServiceImpl<UserMapper,User> implements UserService {
 
+    @Autowired
+    private UserMapper userMapper;
+
+   
+    @Override
+    public User checkUser(String username, String password) {
+        User user = userMapper.findByUsernameAndPassword(username, MD5Utils.code(password));
+        return user;
+    }
 }
