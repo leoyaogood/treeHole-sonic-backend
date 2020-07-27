ada9e5f9f71a7741687a074a1e549188ca7bb8bc
liqiqiorz <1099462011@qq.com>
2020-07-28 02:01:09 +0800

    service注入问题解决

---
+++ src/main/java/com/yxr/weizhi/treehole/entity/Message.java
@@ -0,0 +1,28 @@
+package com.yxr.weizhi.treehole.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author liqiqi_tql
+ * @date 2020/7/24 -22:50
+ */
+public class Message {
+
+
+    private Long id;
+    private String nickname;
+    private String email;
+    private String content;
+    private String avatar;
+    private Date createTime;
+    private Long parentMessageId;
+    private boolean adminMessage;
+
+    //回复留言
+    private List<Message> replyMessages = new ArrayList<>();
+    private Message parentMessage;
+    private String parentNickname;
+
+}
