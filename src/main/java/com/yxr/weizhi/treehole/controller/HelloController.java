ab7822f83a2ce71cc8c81bddea392a48c0753782
liqiqiorz <1099462011@qq.com>
2020-07-29 15:56:40 +0800

    打包时无法找到codeGenerator的jar包,需要除去依赖打包

---
+++ src/main/java/com/yxr/weizhi/treehole/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.yxr.weizhi.treehole.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liqiqi_tql
+ * @date 2020/7/29 -15:43
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello!treeHole's user~~I am liqiqiorz,the author of this project!";
+    }
+}
